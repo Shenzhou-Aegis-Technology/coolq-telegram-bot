c86a4617f8f482758d5a0a547bd2272a8c939466
Milkice <Milkice.Liu+gh@gmail.com>
2017-12-31 18:54:31 +0800

    Switch to effective_*

--- plugins/_1100_text_forward.py
+++ plugins/_1100_text_forward.py
@@ -561,6 +561,9 @@ def handle_forward(context):
     forward_index = get_forward_index(qq_group_id=qq_group_id,
                                       qq_discuss_id=qq_discuss_id)
 
+    if forward_index == -1:
+        return ''
+
     tg_message_id_list = send_from_qq_to_tg(forward_index, message=context['message'],
                                             qq_group_id=qq_group_id,
                                             qq_discuss_id=qq_discuss_id,
